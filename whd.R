e397d88e96421efd8eb1d0157aab0f898a6e67eb
Alan Cobo-Lewis <alanc@maine.edu>
2024-09-08 20:53:20 -0400

    update whd

--- whd.R
+++ whd.R
@@ -9,12 +9,30 @@ whd.tableau.DT <- data.table(read_excel('whd_data/14(c) Certificate Holder Archi
 
 excel.sheets.both <- excel_sheets('whd_data/from archive.org/Both.xlsx')
 excel.sheets.crp <- excel_sheets('whd_data/from archive.org/CRP.xlsx')
-whd.pretableau.both.DT <- map(excel.sheets.both, \(sheet.name) {
+excel.sheets.crp.data.on.num.certs.only <- excel_sheets('whd_data/from archive.org/CRP data on num certs only.xlsx')
+whd.pretableau.both.DT <- (map(excel.sheets.both, \(sheet.name) {
   cbind(list.date=as.Date(gsub('Both ', '', sheet.name)), read_excel('whd_data/from archive.org/Both.xlsx', sheet=sheet.name) %>% select(-contains('Initial')))
-}) %>% rbindlist
+}) %>% rbindlist)[ list.date != '2021-10-01'] # 2021-10-01 already in tableau data table
 whd.pretableau.crp.DT <- map(excel.sheets.crp, \(sheet.name) {
   cbind(list.date=as.Date(gsub('CRP ', '', sheet.name)), read_excel('whd_data/from archive.org/CRP.xlsx', sheet.name))
 }) %>% rbindlist
+whd.pretableau.crp.data.on.num.certs.only.DT <- (map(excel.sheets.crp.data.on.num.certs.only, \(sheet.name) {
+  cbind(list.date=as.Date(sheet.name), read_excel('whd_data/from archive.org/CRP data on num certs only.xlsx', sheet.name) %>% select('State'))
+}) %>% rbindlist)[!is.na(State)]
+
+
+whd.crp.num.by.state.DT <- rbindlist(list(
+  whd.tableau.DT[grep('CRP', `Certificate Type`)][ , .(list.date=as.Date(`List Date`), State)],
+  whd.pretableau.both.DT[grep('CRP', `Certification Type`), .(list.date, State)],
+  whd.pretableau.crp.DT[ , .(list.date, State)],
+  whd.pretableau.crp.data.on.num.certs.only.DT
+))[ , .N, by=.(list.date, State)]
+whd.crp.num.by.state.DT <- whd.crp.num.by.state.DT[CJ(State=unique(whd.crp.num.by.state.DT$State), list.date=unique(whd.crp.num.by.state.DT$list.date)), on=c('State', 'list.date')]
+whd.crp.num.by.state.DT[is.na(N), N := 0]
+
+ggplot(whd.crp.num.by.state.DT[State=='MO'], aes(x=list.date, y=N)) + facet_wrap(~State, scales='free_y') + expand_limits(y=0) + geom_line()
+ggplot(whd.crp.num.by.state.DT[ , .(N=sum(N)), by=list.date], aes(x=list.date, y=N)) + expand_limits(y=0) + geom_line()
+
 
 whd.crp.DT <- rbindlist(list(
   whd.pretableau.both.DT[`Certification Type`=='Community Rehab Program (CRP)', .(
@@ -31,10 +49,19 @@ whd.crp.DT <- rbindlist(list(
 whd.crp.DT[ , .(workers.paid.submin=sum(workers.paid.submin, na.rm=TRUE), certs.issued=length(.I[cert.status=='Issued']), certs.pending=length(.I[cert.status=='Pending'])), by=list.date][order(list.date)]
 whd.crp.DT[ month(list.date)==7, .(workers.paid.submin=sum(workers.paid.submin, na.rm=TRUE), certs.issued=length(.I[cert.status=='Issued']), certs.pending=length(.I[cert.status=='Pending'])), by=list.date][order(list.date)]
 
-workers.paid.submin.DT <- states.dc.pr.us.DT[
-  whd.crp.DT[ month(list.date)==7, .(workers.paid.submin=sum(c(0, workers.paid.submin), na.rm=TRUE), year=year(list.date)), by=.(state, list.date)][order(list.date)], # herehere fix code to have 0 for state/year combos with no certs
-  on=c(geography.abb='state')
+whd.years.DT <- data.table(year=2015:2024)[ , target.date := as.Date(paste0(year, '-07-01'))][]
+whd.years.DT[year==2015, list.date := as.Date(paste0(year, '-10-01'))]
+whd.years.DT[year==2017, list.date := as.Date(paste0(year, '-01-01'))]
+whd.years.DT[!(year %in% c(2015, 2017)), list.date := as.Date(paste0(year, '-07-01'))]
+
+workers.paid.submin.DT <- whd.crp.DT[ , .(workers.paid.submin=sum(c(workers.paid.submin), na.rm=TRUE)), by=.(state, list.date)][order(list.date, state)][
+  whd.years.DT, on='list.date'
 ]
+
+workers.paid.submin.DT <- workers.paid.submin.DT[
+  CJ.dt(states.dc.pr.us.DT[!(geography.abb %in% c('PR', 'US'))], data.table(year=sort(unique(workers.paid.submin.DT$year)))),
+  on=c(state='geography.abb', year='year')
+][is.na(workers.paid.submin), workers.paid.submin := 0][]
 
 pop.by.state.dc.pr.DT <- rbindlist(list(
   data.table(get_estimates(geography='state', variable='POP', breakdown='AGEGROUP', breakdown_labels=TRUE, time_series=TRUE, vintage=2019))[DATE >= 3][ , .(GEOID, NAME, variable, year=DATE-3+2010, pop=value)],
@@ -43,6 +70,83 @@ pop.by.state.dc.pr.DT <- rbindlist(list(
 setorder(pop.by.state.dc.pr.DT, GEOID, year)
 
 workers.paid.submin.per.capita.DT <- pop.by.state.dc.pr.DT[workers.paid.submin.DT, on=c(NAME='geography.name', year='year')][ , workers.paid.submin.per.capita := workers.paid.submin/pop][]
+
+ggplot(
+  workers.paid.submin.per.capita.DT[year==2023],
+  aes(x=year, y=workers.paid.submin.per.capita)
+) + geom_point() + geom_line() + facet_wrap(~state)
+
+
+cor.dataset.DT <- workers.paid.submin.per.capita.DT[,.(NAME, state, year, workers.paid.submin.per.capita)][analysis.dataset.wide.2.DT[NAMEf != 'Puerto Rico' & (cognitive==1 | no.disability==1)], on=c(NAME='state', year='year')][!is.na(workers.paid.submin.per.capita)]
+cor.dataset.DT[ , odds.employed := prop.estimate.employed/(1-prop.estimate.employed)]
+
+cor.dataset.DT[ cognitive==1, cor(workers.paid.submin.per.capita, prop.estimate.employed, method='spearman'), by=year]
+
+cor.dataset.DT[ , cor(workers.paid.submin.per.capita[cognitive==1], prop.estimate.employed[cognitive==1]-prop.estimate.employed[no.disability==1]), by=year]
+
+cor.dataset.DT[ , cor(
+    workers.paid.submin.per.capita[cognitive==1],
+    log(
+      prop.estimate.employed[cognitive==1]/(1-prop.estimate.employed[cognitive==1]) /
+        ( prop.estimate.employed[no.disability==1]/(1-prop.estimate.employed[no.disability==1]) )
+    )
+  ),
+  by=year
+]
+
+ggplot(cor.dataset.DT[cognitive==1], aes(x=(workers.paid.submin.per.capita), y=prop.estimate.employed, label=state)) + geom_text() + facet_wrap(~year)
+ggplot(cor.dataset.DT[cognitive==1], aes(x=sqrt(workers.paid.submin.per.capita), y=prop.estimate.employed, label=state)) + geom_text() + facet_wrap(~year)
+)
+
+gap.DT <- cor.dataset.DT[ , .( workers.paid.submin.per.capita, OR = odds.employed[cognitive==1]/odds.employed[cognitive==0], prop.employed.gap=prop.estimate.employed[no.disability==1]-prop.estimate.employed[cognitive==1]), by=.(state, year)][seq(from=1, to=.N,by=2)]
+ggplot(gap.DT, aes(x=sqrt(workers.paid.submin.per.capita), y=log(OR), label=state)) + geom_text() + facet_wrap(~year)
+ggplot(gap.DT, aes(x=sqrt(workers.paid.submin.per.capita), y=log(OR), group=state)) + geom_path(arrow=arrow(angle=15, ends='last', type='closed', length=unit(5, 'points'))) + facet_wrap(~state)
+gap.DT[ , cor(workers.paid.submin.per.capita, OR, method='spearman'), by=year]
+gap.DT[ , cor(workers.paid.submin.per.capita, OR, method='pearson'), by=year]
+gap.DT[ , cor(sqrt(workers.paid.submin.per.capita), log(OR), method='pearson'), by=year]
+gap.DT[ , cor(sqrt(workers.paid.submin.per.capita), prop.employed.gap, method='pearson'), by=year]
+
+
+
+
+
+fit.cog.0 <- bam(
+  prop.estimate.employed ~
+    s(year) +
+    s(year, by=cognitive) +
+    s(NAMEf, bs='re')
+  # +
+    # s(cognitive, NAMEf, bs='re'),
+  , weights=neff.denom.employed,
+  family=quasibinomial,
+  discrete=TRUE,
+  data=workers.paid.submin.per.capita.DT[,.(NAME, state, year, workers.paid.submin.per.capita)][analysis.dataset.wide.2.DT[NAMEf != 'Puerto Rico' & (cognitive==1 | no.disability==1)], on=.(state, year)]
+)
+fit.cog.1 <- bam(
+  prop.estimate.employed ~
+    s(year) +
+    s(year, by=cognitive) +
+    s(NAMEf, bs='re') +
+    year/workers.paid.submin.per.capita
+  # +
+  # s(cognitive, NAMEf, bs='re'),
+  , weights=neff.denom.employed,
+  family=quasibinomial,
+  discrete=TRUE,
+  data=workers.paid.submin.per.capita.DT[,.(NAME, state, year, workers.paid.submin.per.capita)][analysis.dataset.wide.2.DT[NAMEf != 'Puerto Rico'], on=.(state, year)]
+)
+fit.cog.2 <- bam(
+  prop.estimate.employed ~
+    s(year) +
+    s(year, by=cognitive) +
+    s(NAMEf, bs='re') +
+    s(cognitive, NAMEf, bs='re'),
+  weights=neff.denom.employed,
+  family=quasibinomial,
+  discrete=TRUE,
+  data=workers.paid.submin.per.capita.DT[,.(NAME, state, year, workers.paid.submin.per.capita)][analysis.dataset.wide.2.DT[NAMEf != 'Puerto Rico' & (cognitive==1 | no.disability==1)], on=.(state, year)]
+)
+
 
 ggplot(
   whd.crp.DT[ , .(workers.paid.submin=sum(workers.paid.submin, na.rm=TRUE), certs.issued=length(.I[cert.status=='Issued']), certs.pending=length(.I[cert.status=='Pending'])), by=list.date][order(list.date)],
